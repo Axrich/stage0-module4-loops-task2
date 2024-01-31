8d0fb0f942d58fab32f4083bbc8631c2f206ef46
Axrich <saidahror0101@icloud.com>
2024-01-31 23:01:49 +0500

    .

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,30 +1,14 @@
 package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
-    public void printPrimesUpTo(int upperLimit) {
-        for (int num = 2; num <= upperLimit; num++) {
-            if (isPrime(num)) {
-                System.out.println(num);
+    public void printPrimeNumbers(int printToInclusive) {
+        int primeNumbersBeforePrintToInclusive = 0;
+        while (primeNumbersBeforePrintToInclusive < printToInclusive) {
+            primeNumbersBeforePrintToInclusive++;
+            if (primeNumbersBeforePrintToInclusive % 1 == 0 && primeNumbersBeforePrintToInclusive % 2 == 0) {
+            } else {
+                System.out.println(primeNumbersBeforePrintToInclusive);
             }
         }
-    }
-
-    private boolean isPrime(int number) {
-        if (number <= 1) {
-            return false;
-        }
-
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public static void main(String[] args) {
-        PrimeNumbers primeNumbers = new PrimeNumbers();
-        primeNumbers.printPrimesUpTo(20);
     }
 }
